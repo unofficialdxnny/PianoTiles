9ca622e561929233f785f05a75420de080fa31d9
unofficialdxnny <ahmeddanial324@gmail.com>
2023-07-06 18:33:48 +0100

    .

--- main.py
+++ main.py
@@ -1,33 +1,25 @@
+import win32api, win32con
+import pyautogui as pag
 import time
-import pyautogui
-import keyboard
-import win32con
-import win32api
+import keyboard as kb
 
-tile3_x, tile3_y = 517, 650
-tile2_x, tile2_y = 430, 650
-tile4_x, tile4_y = 606, 650
-tile1_x, tile1_y = 357, 650
-
-time.sleep(5)
-
-print("Script is Running....")
-
-def click(x, y):
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
+def click (x, y):
     win32api.SetCursorPos((x, y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
     time.sleep(0.02)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
+tile2_x, tile2_y = 665,600
+tile3_x, tile3_y = 763,600
+tile4_x, tile4_y = 861,600
+tile1_x, tile1_y = 600,600
 
-while not keyboard.is_pressed("e"):
-    if pyautogui.pixel(tile1_x, tile1_y)[0] == 0:
+time.sleep(3)
+while not kb.is_pressed("e"):
+    if pag.pixel(tile1_x, tile1_y)[0] == 0:
         click(tile1_x, tile1_y + 15)
-
-    if pyautogui.pixel(tile2_x, tile2_y)[0] == 0:
+    if pag.pixel(tile2_x, tile2_y)[0] == 0:
         click(tile2_x, tile2_y + 15)
-
-    if pyautogui.pixel(tile3_x, tile3_y)[0] == 0:
+    if pag.pixel(tile3_x, tile3_y)[0] == 0:
         click(tile3_x, tile3_y + 15)
-
-    if pyautogui.pixel(tile4_x, tile4_y)[0] == 0:
+    if pag.pixel(tile4_x, tile4_y)[0] == 0:
         click(tile4_x, tile4_y + 15)
--- pag.py
+++ pag.py
@@ -2,6 +2,8 @@ import pyautogui as pag
 import keyboard as kb
 
 
+
+print("press enter when over the tile row...")
 if kb.read_key('enter'):
     pos = pag.position()
     print(pos)
